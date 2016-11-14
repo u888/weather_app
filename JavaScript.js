f2589cc6ae5f3864694f5100c4151b2c40c6ca7b
u888 <apisciencebiz@gmail.com>
2016-11-14 22:05:36 +0300

    Update JavaScript.js

--- JavaScript.js
+++ JavaScript.js
@@ -3,11 +3,12 @@ $("document").ready(function () {
         navigator.geolocation.getCurrentPosition(function (position) {
             var lat = position.coords.latitude;
             var lon = position.coords.longitude;
-            var apiurl = '"http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&appid=75b2d6ec47c27f0b9d412ed8f1633dac"'
-
-            $.getJSON(apiurl, function (json) {
-                $("#data").html(JSON.stringify(json));
+            //var apiurl = 
+            
+                $.getJSON("http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=75b2d6ec47c27f0b9d412ed8f1633dac", function (json) {
+                    $("#data").html(JSON.stringify(json));
+                });
             });
-        });
+       
     }
 });
